db70d3f27aebe522dc669a5ca31453e584eb3953
Adejumok <roseadeyinka01@gmail.com>
2022-11-25 17:05:28 +0100

    update

--- code_war.py
+++ code_war.py
@@ -1,6 +1,17 @@
 def returns_odd(arr):
-    temp_arr = []
+    temp_arr = arr[0]
+    odd = 0
+    even = 0
+    odd_num = None
+    even_num = None
     for i in arr:
-        if i % 2 == 0:
-            temp_arr.append(i)
-    return temp_arr
+        if i % 2 != 0:
+            odd += 1
+            odd_num = i
+        else:
+            even += 1
+            even_num = i
+    if odd == 1:
+        return odd_num
+    else:
+        return even_num
--- test_code_war.py
+++ test_code_war.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
+from code_war import returns_odd
+
 
 class Test(TestCase):
     def test_returns_odd(self):
         x = [2, 4, 6, 7, 8, 10]
-        assert(x,7)
-
+        self.assertEqual(returns_odd(x), 7)
